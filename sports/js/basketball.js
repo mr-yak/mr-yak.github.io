18a665f10aa17febb946ee586e5bf78996c8a07e
mr-yak <41585@kristin.school.nz>
2022-05-26 21:28:07 +1200

    Bug Fix

--- sports/js/basketball.js
+++ sports/js/basketball.js
@@ -1,7 +1,7 @@
-import * as THREE from '/three';
+import * as THREE from "../node_modules/three";
 
-import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 var mousex,mousey;
 var ball;
