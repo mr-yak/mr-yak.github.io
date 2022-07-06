1658cbe91aea802f3e459f86d8df953c37c6f720
mr-yak <41585@kristin.school.nz>
2022-07-06 15:08:10 +1200

    added loading screen hack

--- vrassesment/js/preload.js
+++ vrassesment/js/preload.js
@@ -1,1 +1,47 @@
 //preload assets
+var loadingScreen = document.getElementById('load_screen');
+var ctx = loadingScreen.getContext("2d");
+ctx.fillStyle = "#000000";
+ctx.fillRect(0, 0, 1920, 1080);
+ctx.fillText("Loading...", 10, 50);
+
+var thumbnailMap = {
+    0: "#container",
+    1: "#forest",
+    2: "#lagoon",
+    3: "#river",
+    4: "#path",
+    5: "#woods",
+    6: "#seat"
+  };
+
+setTimeout(() => {
+    screenEl.setAttribute('material', 'src',  thumbnailMap[1]);
+}, 1000)
+setTimeout(() => {
+    screenEl.setAttribute('material', 'src',  thumbnailMap[2]);   
+}, 2000)
+
+setTimeout(() => {
+    screenEl.setAttribute('material', 'src',  thumbnailMap[3]);    
+}, 3000)
+
+setTimeout(() => {
+    screenEl.setAttribute('material', 'src',  thumbnailMap[4]);    
+}, 4000)
+
+setTimeout(() => {
+    screenEl.setAttribute('material', 'src',  thumbnailMap[5]);    
+}, 5000)
+
+setTimeout(() => {
+    screenEl.setAttribute('material', 'src',  thumbnailMap[6]);    
+}, 6000)
+
+setTimeout(() => {
+    screenEl.setAttribute('material', 'src',  thumbnailMap[0]);
+}, 7000)
+
+setTimeout(() => {
+    loadingScreen.remove();
+}, 8000)
